78bc7a562bf14fb863343a42e488a4c7e9e43650
kimtaehee <thkim4459@gmail.com>
2026-08-11 14:54:15 +0900

    feat: initial commit

---
+++ src/main/java/tri_lion/health/HealthApplication.java
@@ -0,0 +1,13 @@
+package tri_lion.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tri_lion/health/HealthApplicationTests.java
@@ -0,0 +1,13 @@
+package tri_lion.health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
